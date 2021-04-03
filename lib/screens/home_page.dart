4524b3a13ff300b5a6ab470ce4bcc53865aa506e
redantcode2021 <redantcyber@gmail.com>
2021-04-03 17:25:53 +0700

    Completed Home Page screen

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,7 @@
+import 'package:contoso/tabs/home_tab.dart';
+import 'package:contoso/tabs/saved_tab.dart';
+import 'package:contoso/tabs/search_tab.dart';
+import 'package:contoso/widgets/bottom_tabs.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,10 +12,53 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  PageController _tabsPageController;
+  int _selectedTab = 0;
+
+  @override
+  void initState() {
+    _tabsPageController = PageController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _tabsPageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("HomePage"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: PageView(
+              controller: _tabsPageController,
+              onPageChanged: (num) {
+                setState(() {
+                  _selectedTab = num;
+                });
+              },
+              children: [
+                HomeTab(),
+                SearchTab(),
+                SavedTab(),
+                Center(child: Text("Logout")),
+              ],
+            ),
+          ),
+          BottomTabs(
+            selectedTab: _selectedTab,
+            tabPressed: (num) {
+              _tabsPageController.animateToPage(num,
+                  duration: Duration(milliseconds: 300),
+                  curve: Curves.easeOutCubic);
+            },
+          ),
+        ],
+      ),
     );
   }
 }
