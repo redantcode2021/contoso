e4167458301c508e36668ee68180ddbcc7504048
redantcode2021 <redantcyber@gmail.com>
2021-04-03 16:35:08 +0700

    Completed bottom tabs

--- lib/widgets/bottom_tabs.dart
+++ lib/widgets/bottom_tabs.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 
-class BottomTabs extends StatelessWidget {
+class BottomTabs extends StatefulWidget {
+  final int selectedTab;
+  final Function(int) tabPressed;
+
+  const BottomTabs({Key key, this.selectedTab, this.tabPressed})
+      : super(key: key);
+  @override
+  _BottomTabsState createState() => _BottomTabsState();
+}
+
+class _BottomTabsState extends State<BottomTabs> {
+  int _selectedTab = 0;
   @override
   Widget build(BuildContext context) {
+    _selectedTab = widget.selectedTab ?? 0;
     return Container(
       decoration: BoxDecoration(
         color: Colors.white,
@@ -12,7 +24,7 @@ class BottomTabs extends StatelessWidget {
         ),
         boxShadow: [
           BoxShadow(
-            color: Colors.black,
+            color: Colors.black.withOpacity(0.05),
             spreadRadius: 1.0,
             blurRadius: 30.0,
           )
@@ -23,15 +35,31 @@ class BottomTabs extends StatelessWidget {
         children: [
           BottomTabsButton(
             imagePath: 'assets/images/tab_home.png',
+            selected: _selectedTab == 0 ? true : false,
+            onPressed: () {
+              widget.tabPressed(0);
+            },
           ),
           BottomTabsButton(
             imagePath: 'assets/images/tab_search.png',
+            selected: _selectedTab == 1 ? true : false,
+            onPressed: () {
+              widget.tabPressed(1);
+            },
           ),
           BottomTabsButton(
             imagePath: 'assets/images/tab_saved.png',
+            selected: _selectedTab == 2 ? true : false,
+            onPressed: () {
+              widget.tabPressed(2);
+            },
           ),
           BottomTabsButton(
             imagePath: 'assets/images/tab_logout.png',
+            selected: _selectedTab == 3 ? true : false,
+            onPressed: () {
+              widget.tabPressed(3);
+            },
           ),
         ],
       ),
@@ -40,23 +68,41 @@ class BottomTabs extends StatelessWidget {
 }
 
 class BottomTabsButton extends StatelessWidget {
-  const BottomTabsButton({Key key, this.imagePath, this.selected})
+  const BottomTabsButton(
+      {Key key, this.imagePath, this.selected, this.onPressed})
       : super(key: key);
 
   final String imagePath;
   final bool selected;
+  final Function onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(
-        vertical: 24.0,
-        horizontal: 16.0,
-      ),
-      child: Image(
-        image: AssetImage(imagePath ?? 'assets/images/tab_home.png'),
-        width: 22.0,
-        height: 22.0,
+    bool _selected = selected ?? false;
+
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: 24.0,
+          horizontal: 16.0,
+        ),
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: _selected
+                  ? Theme.of(context).accentColor
+                  : Colors.transparent,
+              width: 2.0,
+            ),
+          ),
+        ),
+        child: Image(
+          image: AssetImage(imagePath ?? 'assets/images/tab_home.png'),
+          width: 22.0,
+          height: 22.0,
+          color: _selected ? Theme.of(context).accentColor : Colors.black,
+        ),
       ),
     );
   }
